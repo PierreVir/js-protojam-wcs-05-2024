8d649712e7941ac3eb2c39c563bf8c22b58154ec
PierreVir <pierre.virama@gmail.com>
2024-05-16 14:54:06 +0200

    first commit

---
+++ src/components/Hobbies.jsx
@@ -0,0 +1,28 @@
+function Hobbies (){
+
+
+    return(
+        <>
+
+        <div>
+            <p>
+                Comme Chuchill Cherchez vous une ou plusieurs activités qui vous permettra de deconnecté de ce monde bruyant 
+                afin d'avoir l'esprit plus clair, être en harmonie avec ses propres choix et décision.
+                on peut tous reconnaitre cette partie de Churchill en nous qui ont été un acharné du travail où on ne 
+                s'arrete jamais ! <br />
+                Alors prenez le temps pour vous et de vous deconnecté ce voyage ce veux pas visuelle mais manuelle qui vous 
+                donnera la place à votre déconnexion !
+            </p>
+        </div>
+
+        <div>
+            <h2>Choisit Ton hobbies </h2>
+        </div>
+
+
+        </>
+    )
+
+}
+
+export default Hobbies;
